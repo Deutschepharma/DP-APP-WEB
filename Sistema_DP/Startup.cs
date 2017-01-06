c690aa0ade0acc7dd825b417fcb4d4db3c4b7824
AbdonKun <abdon.sandoval@gmail.com>
2017-01-06 10:54:01 -0300

    first commit

---
+++ Sistema_DP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sistema_DP.Startup))]
+namespace Sistema_DP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
